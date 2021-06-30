c65690988c67a61186f49cb93516ce6efcb29b16
Maykon G Pedro <maykonglaffite@gmail.com>
2021-06-29 22:03:44 -0300

    add comentários no exemplo 1

--- exemplos_de_aula/01-pokemon.R
+++ exemplos_de_aula/01-pokemon.R
@@ -5,43 +5,76 @@ library(jsonlite)
 u_base <- "https://pokeapi.co/api/v2/"
 endpoint <- "/pokemon/ditto"
 u_pokemon <- paste0(u_base, endpoint)
-r_pokemon <- GET(u_pokemon)
+r_pokemon <- httr::GET(u_pokemon)
 
-# 1. Entenda a diferença entre essas formas de pegar o conteúdo do resultado
+
+
+# 1. Entenda a dif. entre essas formas de pegar o conteúdo do resultado -------
+
 
 # Solta uma lista de informações sobre o pokemon escolhido (ditto)
-content(r_pokemon)
+httr::content(r_pokemon)
+
+# Solta um resultado em forma de texto (difícil utilização)
+httr::content(r_pokemon, "text")
+
+# Solta um resultado em forma de uso de memória, seu uso é raro
+httr::content(r_pokemon, "raw")
+
+# Solta a lista padrão de infos. Basicamente ela roda a função jsonlite::fromJSON
+# para transformar o "text" em uma lista organizada.
+httr::content(r_pokemon, "parsed")
+
+# Retorna a lista simplificada em um dataframe
+httr::content(r_pokemon, "parsed", simplifyDataFrame = TRUE)
+
+# Essa é a fórmula que é rodada dentrto do "content"
+httr::content(r_pokemon, "text") |> 
+  jsonlite::fromJSON()
 
 
 
-content(r_pokemon, "text")
-content(r_pokemon, "raw")
-content(r_pokemon, "parsed")
 
-content(r_pokemon, "parsed", simplifyDataFrame = TRUE)
+# 2. Pegar informações filtrando com o código -----------------------------
 
-fromJSON(content(r_pokemon, "text"))
 
-# 2. Pegar informações filtrando com o código
-
+# Definindo itens para a query
 q_pokemon <- list(
+  
+  # queremos 8 pokemons por vez
   limit = 8, 
+  
+  # pulando a primeira geração
   offset = 1
 )
 
-r_pokemon_filtrado <- GET(
+
+# Fazendo uma query
+r_pokemon_filtrado <- httr::GET(
   paste0(u_base, "/pokemon"), 
+  
+  # essa informação adiciona mais itens na url para seguir o filtro que definimos
   query = q_pokemon
 )
 
-content(r_pokemon_filtrado, simplifyDataFrame = TRUE)
 
-# 3. O que faz httr::write_disk()?
+# Acessa o conteúdo da query
+httr::content(r_pokemon_filtrado, simplifyDataFrame = TRUE)
 
+
+
+
+# 3. O que faz httr::write_disk()? ----------------------------------------
+
+# Criar pasta de output no projeto
 dir.create("output", showWarnings = FALSE, recursive = TRUE)
 
-GET(
+# Salvar a página da web/api em json no computador
+httr::GET(
   paste0(u_base, "/pokemon"), 
   query = q_pokemon, 
-  write_disk("output/01-pokemon.json")
+  httr::write_disk("output/01-pokemon.json")
 )
+
+# Esse backup pode ser acessado pela função abaixo:
+jsonlite::read_json("output/01-pokemon.json")
